2760dc30c1b0902e84ea529cac7e233263c9ccec
hieuhumg <hieu.nikon2711@gmail.com>
2020-03-18 07:34:15 +0700

    a

--- lesson-18/shopping/index.php
+++ lesson-18/shopping/index.php
@@ -8,7 +8,16 @@ get_header();
 ?>
 
 <?php
-    showData($list_product);
+    $mod = !empty($_GET['mod']) ? $_GET['mod'] : 'home';
+    $act = !empty($_GET['act']) ? $_GET['act'] : 'main';
+
+    $path = "modules/{$mod}/{$act}.php";
+
+    if(file_exists($path)) {
+        require $path;
+    }else {
+        require "inc/404.php";
+    }
 ?>
 
 <?php
